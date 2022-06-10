957540ce1755e1904d5f33c7a93b93c4a68a5b94
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-06-10 19:13:20 +0200

    set up directory layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
+import { StrengthPipe } from './strength/strength.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroComponent,
+    HeroDetailComponent,
+    HeroSearchComponent,
+    HeroesComponent,
+    MessagesComponent,
+    StrengthPipe,
   ],
   imports: [
     BrowserModule,
