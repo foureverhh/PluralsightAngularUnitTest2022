ebf217073a7d528ba25a11814bc506b1b6a8e1a9
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-06-13 00:21:31 +0200

    Add FakeHeroComponent as child component to HeroesComponent

---
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,51 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesComponent } from './heroes.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+import { of } from 'rxjs';
+// shallow test with child component and service dependency
+describe('heroes component (shallow test demo with child component and service dependency)', ()=> {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let HEROES: Hero[];
+
+  //mock HeroService dependency
+  let mockHeroService: any;
+
+  //fake a HeroComponent as <app-hero></app-hero>
+  @Component({
+  selector: 'app-hero',
+  template: '<div></div>',
+  })
+  class FakeHeroComponent {
+    @Input() hero!: Hero;
+  }
+
+
+  beforeEach(()=> {
+    mockHeroService = jasmine.createSpyObj(['addHero', 'getHeroes', 'deleteHero']);
+    HEROES = [
+      {id: 1, name:'name1', strength:1},
+      {id: 2, name:'name2', strength:2},
+      {id: 3, name:'name3', strength:3}
+    ];
+    TestBed.configureTestingModule({
+      declarations: [ 
+        HeroesComponent,
+        FakeHeroComponent 
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ]
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly', ()=> {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges(); //to call angular lifecycle, so onInit() heroService.getHeroes() is called. No need to call fixture.componentInstance.heroes = HEROES;
+    // fixture.componentInstance.heroes = HEROES;
+    expect(fixture.componentInstance.heroes.length).toEqual(HEROES.length);
+  });
+});
