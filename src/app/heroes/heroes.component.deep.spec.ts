fef22e2d83f1ce149770785f1468c2a1a1f49f11
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-06-28 23:07:33 +0200

    5.3 Finding Elements by Directive

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -5,6 +5,7 @@ import { HeroComponent } from '../hero/hero.component';
 import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { of } from 'rxjs';
 import { Hero } from '../hero';
+import { By } from '@angular/platform-browser';
 describe('deep test with real child component', ()=>{
     let mockHeroService: any;
     let HEROES: Hero[];
@@ -24,12 +25,26 @@ describe('deep test with real child component', ()=>{
             {id: 2, name:'name2', strength:2},
             {id: 3, name:'name3', strength:3}
         ];
-        mockHeroService.getHeroes.and.returnValue(of(HEROES));
-        fixture.detectChanges();
+        
+        
     });
 
     it('should be true', () => {
-        
         expect(true).toBe(true);
+    });
+
+    it('should render each hero as a HeroComponent', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        // run ngOnInit
+        fixture.detectChanges();
+        // component actual is a directive, directive is the parent class to compoent, so use By.directive 
+        // find all HeroComponent
+        let heroComponentsDebugElement = fixture.debugElement.queryAll(By.directive(HeroComponent));
+        expect(heroComponentsDebugElement.length).toEqual(3);
+        let firstHeroComponent = heroComponentsDebugElement[0].componentInstance;
+        expect(firstHeroComponent.hero).toEqual(HEROES[0]);
+        for(let i=0; i<heroComponentsDebugElement.length;i++) {
+            expect(heroComponentsDebugElement[i].componentInstance.hero).toEqual(HEROES[i]);
+        }
     });
 })
