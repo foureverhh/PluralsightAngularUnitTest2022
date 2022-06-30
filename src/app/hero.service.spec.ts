702429984d665ab8b09e2e2325a3a0bdd8de258a
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-06-30 23:40:33 +0200

    5.5 Implementting a Test with Mocked resutl from HttpTestingController

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -7,6 +7,7 @@ describe('HeroSerice Test', ()=>{
     let mockMessageService;
     let httpTestingController: HttpTestingController;
     let heroServiceAgent: HeroService; // The handle to reach HeroService in providers in TestBed *
+    let heroesUrl = 'api/heroes';  
 
     beforeEach(()=>{
         mockMessageService = jasmine.createSpyObj(["add"]);
@@ -26,17 +27,30 @@ describe('HeroSerice Test', ()=>{
     describe('getHero', ()=>{
         it('should call get with the correct URL', () => {
             // call getHero()
-            heroServiceAgent.getHero(4);
+            heroServiceAgent.getHero(4).subscribe(hero => {
+                expect(hero.id).toBe(4);
+                expect(hero).toEqual({id:4, name:'SuperDude', strength: 100});
+            }); //to subscribe the result of getHero(id) observable, actually the mock result from req.flush();
+            
             // test that the URL was correct
-            httpTestingController.
+            const req = httpTestingController.expectOne(heroesUrl + '/4');
+            req.flush({id:4, name:'SuperDude', strength: 100}) //mock return responce for request
+            expect(req.request.method).toBe('GET'); //check request method is GET
+            httpTestingController.verify();
         });
 
         it('should call get with the correct URL with inside inject', inject(
             [HeroService, HttpTestingController], 
             (service: HeroService, controller: HttpTestingController) => {
-            // call getHero()
-            service.getHero(4);
+            // call getHero(), and subscribe() to call the http.get()
+            service.getHero(1).subscribe(hero => {
+                expect(hero).toEqual({id:1, name:'haha', strength:100}); //check result from http is correct
+            }); 
             // test that the URL was correct
+            let testRequest = controller.expectOne(heroesUrl + '/1'); // to check right url is called
+            testRequest.flush({id:1, name:'haha', strength:100}) //mock responce from testRequest
+            expect(testRequest.request.method).toBe('GET'); // to check http method as GET
+            controller.verify(); 
         }));
     });
 })
