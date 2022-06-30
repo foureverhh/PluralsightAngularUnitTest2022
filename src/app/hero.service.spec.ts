dc0107d7a6ca20212beb8cff80ee2e0ae5e11ac9
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-06-30 22:55:45 +0200

    5.4 Using the inject Helper Function to inject Service and Controller in unit test callback function

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
@@ -6,19 +6,37 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 describe('HeroSerice Test', ()=>{
     let mockMessageService;
     let httpTestingController: HttpTestingController;
+    let heroServiceAgent: HeroService; // The handle to reach HeroService in providers in TestBed *
 
     beforeEach(()=>{
         mockMessageService = jasmine.createSpyObj(["add"]);
         TestBed.configureTestingModule({
             imports:[ HttpClientTestingModule ],
             providers:[
-                HeroService,
+                HeroService, //*
                 { provide: MessageService, useValue:mockMessageService }
             ],
         });
 
         httpTestingController = TestBed.inject(HttpTestingController); //inject httpTestingController to TestBed
-        // let heroServiceInjected = TestBed.inject(HeroService);
+        heroServiceAgent = TestBed.inject(HeroService);
         // let messageServiecInjected = TestBed.inject(MessageService);
-    })
+    });
+
+    describe('getHero', ()=>{
+        it('should call get with the correct URL', () => {
+            // call getHero()
+            heroServiceAgent.getHero(4);
+            // test that the URL was correct
+            httpTestingController.
+        });
+
+        it('should call get with the correct URL with inside inject', inject(
+            [HeroService, HttpTestingController], 
+            (service: HeroService, controller: HttpTestingController) => {
+            // call getHero()
+            service.getHero(4);
+            // test that the URL was correct
+        }));
+    });
 })
