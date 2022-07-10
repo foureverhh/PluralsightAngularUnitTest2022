b4e01481a2f7650667504f5f22a76c94d91f0f08
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-07-10 11:52:53 +0200

    6.4 Raising Events on Child Directives

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -57,9 +57,38 @@ describe('deep test with real child component', ()=>{
         const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
         // find the button on the first HeroComponent instance, and the click event
         heroComponents[0].query(By.css("button")).triggerEventHandler('click', {stopPropagation: () => {}});
+        // trigger delete from child component
         // (<HeroComponent> heroComponents[0].componentInstance).delete.emit(HEROES[0]); // Comvert heroComponents[0].componentInstance to HeroComponent
         // (<HeroComponent> heroComponents[0].componentInstance).delete.emit(undefined);
+
         heroComponents[0].triggerEventHandler('delete', null); // same trggering delete event on HeroCompont, but this is from parent debugElement
         expect(fixture.componentInstance.deleteHero).toHaveBeenCalledWith(HEROES[0]); //HEROES[0] is as parameter when delete is called
+    })
+
+    it('shoudl add a new hero to the hero list when the add button is clicked', () => {
+        mockHeroService.getHeroes.and.returnValue(of(HEROES));
+        // run ngOnInit
+        fixture.detectChanges();
+        const newHeroName = "Mr. Ice";
+        mockHeroService.addHero.and.returnValue(of({ id:"5", name:newHeroName, strength: 4}));
+        
+        const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+        const addButton = fixture.debugElement.queryAll(By.css('button'))[0]; 
+
+
+        // // mock input element get value by typing Mr.ice to input
+        inputElement.value = newHeroName;
+        addButton.triggerEventHandler('click', null); // as this click will call add() method, that is why null as eventObject, because we do not really use that one
+        
+        fixture.detectChanges();
+        // alternative 1 find Jquery() textContent of ul
+        const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
+        expect(heroText).toContain(newHeroName);
+        // alternative 2 find Jquery() innerText of li
+        const heroLis = fixture.debugElement.queryAll(By.css('li'));
+        expect(heroLis[3].nativeElement.innerText).toContain(newHeroName);
+        // alternative 3 
+        const heroElements = fixture.debugElement.queryAll(By.directive(HeroComponent));
+        expect(heroElements[3].nativeElement.innerText).toContain(newHeroName);
     })
 })
